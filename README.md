0bb56f06ef9655d0c77fa61f0ab905a06177bcee
EyreFree <zheng.xu03@ele.me>
2017-11-10 14:46:54 +0800

    Setup

---
+++ README.md
@@ -0,0 +1,21 @@
+# TankBattle
+
+饿了么-百度外卖 2017 黑客马拉松初赛
+
+## 概述
+
+[https://hackathon.ele.me/?introduce.html](https://hackathon.ele.me/?introduce.html)
+
+## 环境
+
+- XCode 9.0+
+- Swift 4.0+
+
+## 构建
+
+1. 用 XCode 打开 TankBattle.xcodeproj；
+2. 构建。
+
+## 协议
+
+不存在的，TankBattle 为内部使用，请勿外泄。
---
+++ TankBattle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TankBattle
+//
+//  Created by EyreFree on 2017/11/10.
+//  Copyright © 2017年 MeituanFucker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
